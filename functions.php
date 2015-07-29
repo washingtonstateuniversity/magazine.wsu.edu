fd95c637f5bdc4bf8f2ea99ef70fc37a7d2f6051
jeremyfelt <jeremy.felt@gmail.com>
2015-07-29 16:57:42 -0700

    Add initial framework for Magazine theme

---
+++ functions.php
@@ -0,0 +1,39 @@
+<?php
+
+class WSU_Magazine_Theme {
+	/**
+	 * @var WSU_Magazine_Theme
+	 */
+	private static $instance;
+
+	/**
+	 * Maintain and return the one instance and initiate hooks when
+	 * called the first time.
+	 *
+	 * @return \WSU_Magazine_Theme
+	 */
+	public static function get_instance() {
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new WSU_Magazine_Theme;
+			self::$instance->load_plugins();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Load "plugins" included with the theme.
+	 */
+	public function load_plugins() {
+
+	}
+}
+
+add_action( 'after_setup_theme', 'WSU_Magazine_Theme' );
+/**
+ * Start things up.
+ *
+ * @return \WSU_Magazine_Theme
+ */
+function WSU_Magazine_Theme() {
+	return WSU_Magazine_Theme::get_instance();
+}
