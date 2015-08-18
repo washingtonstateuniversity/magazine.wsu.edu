04330ab08c93c26d6502739642fef970867ac862
Stephen Locker <slocker@Stephens-MacBook-Air.local>
2015-08-18 15:16:07 -0700

    archive layout and style adjustments

---
+++ parts/after-main.php
@@ -0,0 +1,17 @@
+<footer class="main-footer">
+				<nav class="magazine-nav">
+					<a href="https://magazine.wsu.edu">Current Issue</a>
+					<a href="https://magazine.wsu.edu/about">About <em>WSM</em></a>
+					<a href="https://magazine.wsu.edu/advertising">Advertising</a>
+					<a href="https://magazine.wsu.edu/contact">Contact</a>
+					<a href="http://wsm.wsu.edu/ourstory/index.php?title=Main_Page">Our Story</a>
+				</nav>
+				<div class="wsu-logo"><img src="https://repo.wsu.edu/spine/develop/marks/wsu-signature-horizontal.svg" alt="Washington State University"></div>
+				<nav class="wsu-nav">
+					<a href="https://wsu.edu">WSU Home</a>
+					<a href="https://news.wsu.edu">News</a>
+					<a href="http://alumni.wsu.edu">Alumni/Friends</a>
+					<a href="http://wsucougars.com">Athletics</a>
+					<a href="https://foundation.wsu.edu">Give to WSU</a>
+				</nav>
+			</footer>
