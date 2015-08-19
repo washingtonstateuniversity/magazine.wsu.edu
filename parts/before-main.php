3cb05ade98f051ddf06d6b88ee23248044c98781
Stephen Locker <slocker@Stephens-MacBook-Air.local>
2015-08-18 21:18:26 -0700

    added nav to site header

--- parts/before-main.php
+++ parts/before-main.php
@@ -6,6 +6,12 @@
 		<div class="site-header-issue sh-item">
 			<?php echo esc_html( magazine_get_issue_name() ); ?>
 		</div>
-		<div class="site-header-nav sh-item"></div>
+		<nav class="site-header-nav sh-item">
+			<a href="https://stage.magazine.wsu.edu/e-magazine/">eMag</a>
+			<a href="https://stage.magazine.wsu.edu/video/">Media</a>
+			<a href="#">Search</a>
+			<a href="https://stage.magazine.wsu.edu/past-issues/">Issues</a>
+			<a href="#">Connect</a>
+		</nav>
 		<div class="site-header-logo sh-item"><img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/WSM_logo.svg' ); ?>" alt="Washington State Magazine"></div>
 	</header>
