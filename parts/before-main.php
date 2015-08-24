53dd738467221a6baf87aaf3a75493f5503654d9
Stephen Locker <slocker@Stephens-MacBook-Air.local>
2015-08-23 20:45:08 -0700

    add navigation icons and site nav classes, plus style adjustments

--- parts/before-main.php
+++ parts/before-main.php
@@ -7,11 +7,11 @@
 			<?php echo esc_html( magazine_get_issue_name() ); ?>
 		</div>
 		<nav class="site-header-nav sh-item">
-			<a href="https://stage.magazine.wsu.edu/e-magazine/">eMag</a>
-			<a href="https://stage.magazine.wsu.edu/video/">Media</a>
-			<a href="#">Search</a>
-			<a href="https://stage.magazine.wsu.edu/past-issues/">Issues</a>
-			<a href="#">Connect</a>
+			<a class="nav-emag" href="https://stage.magazine.wsu.edu/e-magazine/">eMag</a>
+			<a class="nav-media" href="https://stage.magazine.wsu.edu/video/">Media</a>
+			<a class="nav-search" href="#">Search</a>
+			<a class="nav-issues" href="https://stage.magazine.wsu.edu/past-issues/">Issues</a>
+			<a class="nav-connect" href="#">Connect</a>
 		</nav>
-		<div class="site-header-logo sh-item"><img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/WSM_logo.svg' ); ?>" alt="Washington State Magazine"></div>
+		<div class="site-header-logo sh-item"><a href="https://stage.magazine.wsu.edu"><img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/WSM_logo.svg' ); ?>" alt="Washington State Magazine"></a></div>
 	</header>
