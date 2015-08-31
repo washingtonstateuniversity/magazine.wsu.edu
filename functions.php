d8e3572cced80dba7d377f23c6ae0771f1a11f98
jeremyfelt <jeremy.felt@gmail.com>
2015-08-31 12:52:38 -0700

    Adjust allowed file types to support SVG for Admin and Editor roles

--- functions.php
+++ functions.php
@@ -40,6 +40,8 @@ class WSU_Magazine_Theme {
 	public function setup_hooks() {
 		add_action( 'admin_init', array( $this, 'editor_style' ) );
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		add_filter( 'pre_site_option_upload_filetypes', array( $this, 'set_upload_filetypes' ), 11, 1 );
+		add_filter( 'upload_mimes', array( $this, 'set_mime_types' ), 11, 1 );
 	}
 
 	public function editor_style() {
@@ -49,6 +51,36 @@ class WSU_Magazine_Theme {
 	public function enqueue_scripts() {
 		wp_enqueue_script( 'wsu-magazine-typekit', 'https://use.typekit.net/auo5wsi.js', array(), false, false );
 		wp_enqueue_script( 'wsu-magainze-typekit-load', get_stylesheet_directory_uri() . '/js/magazine-primary.js', array(), false, true );
+	}
+
+	/**
+	 * Adjust the allowed file types to support SVG if a user is an administrator or editor.
+	 *
+	 * @param string $upload_filetypes
+	 *
+	 * @return string
+	 */
+	public function set_upload_filetypes( $upload_filetypes ) {
+		if ( current_user_can( 'administrator' ) || current_user_can( 'editor' ) ) {
+			$upload_filetypes .= ' svg';
+		}
+
+		return $upload_filetypes;
+	}
+
+	/**
+	 * Adjust the allowed mime types to support SVG if a user is an administrator or editor.
+	 *
+	 * @param array $mime_types
+	 *
+	 * @return array
+	 */
+	public function set_mime_types( $mime_types ) {
+		if ( current_user_can( 'administrator' ) || current_user_can( 'editor' ) ) {
+			$mime_types['svg'] = 'image/svg+xml';
+		}
+
+		return $mime_types;
 	}
 }
 
