139fb372f9d4750730d4557b68dd607d3e858624
Stephen Locker <slocker@wsu.edu>
2015-09-01 17:00:32 -0700

    top nav work

--- parts/before-main.php
+++ parts/before-main.php
@@ -2,16 +2,74 @@
 	//this is where the top site header content will be populated
 ?>
 
-	<header class="site-header">
-		<div class="site-header-issue sh-item">
-			<?php echo esc_html( magazine_get_issue_name() ); ?>
-		</div>
-		<nav class="site-header-nav sh-item">
-			<a class="nav-emag" href="https://stage.magazine.wsu.edu/e-magazine/">eMAG</a>
-			<a class="nav-media" href="https://stage.magazine.wsu.edu/video/">Media</a>
-			<a class="nav-search" href="#">Search</a>
-			<a class="nav-issues" href="https://stage.magazine.wsu.edu/past-issues/">Issues</a>
-			<a class="nav-connect" href="#">Connect</a>
-		</nav>
-		<div class="site-header-logo sh-item"><a href="https://stage.magazine.wsu.edu"><img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/WSM_logo.svg' ); ?>" alt="Washington State Magazine"></a></div>
-	</header>
+    <header class="site-header">
+        <div class="site-header-issue sh-item">
+            <?php echo esc_html( magazine_get_issue_name() ); ?>
+        </div>
+        <nav class="site-header-nav sh-item">
+            <a class="nav-emag" href="https://stage.magazine.wsu.edu/e-magazine/"><span><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="21.086px" height="15.968px" viewBox="0 0 21.086 15.968" enable-background="new 0 0 21.086 15.968" xml:space="preserve">
+<path d="M20.669,0.393c-3.28-0.52-6.579-0.523-9.862-0.015C7.524-0.13,4.005-0.126,0.725,0.393L0,0.487v15.481l0.989-0.129
+	c3.172-0.502,6.646-0.502,9.818,0c3.172-0.502,6.425-0.502,9.599,0l0.68,0.129V0.487L20.669,0.393z M5.623,14.054
+	c-1.406,0-2.812,0.094-4.217,0.277V1.694c2.812-0.386,6.326-0.386,8.435,0v12.638C9.138,14.148,7.029,14.054,5.623,14.054z
+	 M19.681,14.332c-0.702-0.184-2.812-0.277-4.218-0.277c-1.405,0-2.811,0.094-4.217,0.277V1.694c2.812-0.386,6.326-0.386,8.435,0
+	V14.332z"/>
+</svg>
+ </span>eMAG</a>
+            <a class="nav-media" href="https://stage.magazine.wsu.edu/video/">
+                <span><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="18.611px" height="14.486px" viewBox="0 0 18.611 14.486" enable-background="new 0 0 18.611 14.486" xml:space="preserve">
+<g>
+	<path d="M9.325,14.486c-0.006,0-0.013,0-0.02,0s-0.013,0-0.019,0H9.325z"/>
+	<path d="M9.287,0c0.006,0,0.012,0,0.019,0s0.013,0,0.019,0H9.287z"/>
+	<circle cx="9.371" cy="7.491" r="2.693"/>
+	<path d="M18.608,8.078c-0.012-1.005-0.061-2.013-0.201-3.01c-0.1-0.71-0.131-1.757-0.904-2.148
+		c-0.537-0.273-1.486-0.244-2.092-0.295c-0.555-0.046-1.25,0.078-1.609-0.352c-0.287-0.341-0.498-0.735-0.689-1.126
+		c-0.316-0.647-0.309-0.831-1.143-0.963C11.097,0.046,10.199,0,9.305,0S7.513,0.046,6.641,0.184
+		C5.806,0.315,5.814,0.499,5.498,1.146C5.306,1.537,5.095,1.932,4.809,2.272C4.448,2.702,3.753,2.578,3.2,2.624
+		C2.593,2.675,1.645,2.646,1.106,2.919C0.334,3.311,0.302,4.357,0.202,5.067c-0.14,0.997-0.188,2.005-0.199,3.01
+		c-0.012,0.973,0.016,1.947,0.104,2.916c0.068,0.753,0.075,1.854,0.494,2.521c0.632,1.007,3.302,0.83,4.387,0.879
+		c1.438,0.064,2.878,0.092,4.318,0.092c1.44,0,2.881-0.027,4.319-0.092c1.084-0.049,3.754,0.128,4.385-0.879
+		c0.42-0.668,0.428-1.769,0.496-2.521C18.591,10.025,18.62,9.05,18.608,8.078z M9.371,11.974c-2.476,0-4.483-2.008-4.483-4.483
+		c0-2.476,2.007-4.483,4.483-4.483s4.482,2.007,4.482,4.483C13.853,9.966,11.847,11.974,9.371,11.974z"/>
+</g>
+</svg>
+</span> Media
+            </a>
+            <a class="nav-search" href="#">
+                <span><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="15.262px" height="14.884px" viewBox="0 0 15.262 14.884" enable-background="new 0 0 15.262 14.884" xml:space="preserve">
+<path d="M4.891,1.779C2.953,3.717,2.6,6.637,3.827,8.935l-3.388,3.388c-0.586,0.586-0.586,1.536,0,2.122s1.535,0.586,2.121,0
+	l3.291-3.291c2.357,1.552,5.557,1.291,7.631-0.783c2.373-2.373,2.373-6.219,0-8.592C11.109-0.594,7.264-0.593,4.891,1.779z
+	 M12.436,9.324c-1.795,1.795-4.703,1.795-6.498,0c-1.795-1.794-1.795-4.704,0-6.498s4.703-1.794,6.498,0S14.23,7.529,12.436,9.324z"
+	/>
+</svg>
+</span> Search
+            </a>
+            <a class="nav-issues" href="https://stage.magazine.wsu.edu/past-issues/">
+                <span><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="17px" height="17px" viewBox="0 0 17 17" enable-background="new 0 0 17 17" xml:space="preserve">
+<g>
+	<g>
+		<path d="M6,17h11V4H6V17z"/>
+	</g>
+	<polygon points="2,1 11,1 11,0 0,0 0,13 2,13 	"/>
+	<polygon points="5,3 14,3 14,2 3,2 3,15 5,15 	"/>
+</g>
+</svg></span> Issues
+            </a>
+            <a class="nav-connect" href="#">
+                <span><svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="13.652px" height="14.62px" viewBox="0 0 13.652 14.62" enable-background="new 0 0 13.652 14.62" xml:space="preserve">
+<path d="M11.178,9.671c-0.6,0-1.15,0.213-1.578,0.568L4.938,7.548C4.944,7.47,4.949,7.39,4.949,7.31S4.944,7.15,4.938,7.072
+	L9.6,4.381c0.428,0.355,0.979,0.569,1.578,0.569c1.367,0,2.475-1.108,2.475-2.475C13.652,1.108,12.545,0,11.178,0
+	C9.812,0,8.703,1.108,8.703,2.475c0,0.179,0.02,0.353,0.056,0.521l-4.502,2.6c-0.45-0.468-1.082-0.76-1.782-0.76
+	C1.107,4.835,0,5.943,0,7.31s1.107,2.475,2.475,2.475c0.7,0,1.332-0.292,1.782-0.761l4.502,2.6
+	c-0.036,0.168-0.056,0.342-0.056,0.521c0,1.367,1.108,2.475,2.475,2.475c1.367,0,2.475-1.107,2.475-2.475S12.545,9.671,11.178,9.671
+	z"/>
+</svg>
+</span> Connect
+            </a>
+        </nav>
+        <div class="site-header-logo sh-item"><a href="https://stage.magazine.wsu.edu"><img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/WSM_logo.svg' ); ?>" alt="Washington State Magazine"></a></div>
+    </header>
