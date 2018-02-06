a6f494de02d0212e5cd14857bc1d72ca9319d373
Jeremy Felt <jeremy.felt@gmail.com>
2018-02-06 10:32:02 -0800

    Add aria-hidden=true to single page author display

--- parts/headers.php
+++ parts/headers.php
@@ -24,13 +24,19 @@ if ( spine_get_option( 'main_header_show' ) == 'true' ) :
 	if ( ! $featured_image_position || sanitize_html_class( $featured_image_position ) !== $featured_image_position ) {
 		$featured_image_position = 'background-position-center';
 	}
+
+	// We use opacity:0 on single pages to hide the author, so hide it from screen readers too.
+	$aria_author = '';
+	if ( is_singular( 'page' ) ) {
+		$aria_author = 'aria-hidden="true"';
+	}
 ?>
 	<header class="main-header <?php echo $featured_image_position; ?>">
 		<div class="header-group hgroup">
 			<h1 class="h1-header" data-sitename="<?php echo $spine_main_header_values['site_name']; ?>" data-pagetitle="<?php echo $spine_main_header_values['page_title']; ?>" data-posttitle="<?php echo $spine_main_header_values['post_title']; ?>" data-default="<?php echo esc_html( $spine_main_header_values['sub_header_default'] ); ?>" data-alternate="<?php echo esc_html( $spine_main_header_values['sub_header_alternate'] ); ?>"><?php echo strip_tags( $spine_main_header_values['sub_header_default'], '<a>' ); ?></h1>
 
 			<?php if ( is_singular() ) : ?>
-			<cite class="article-author" role="author">by <?php coauthors_posts_links(); ?></cite>
+			<cite class="article-author" <?php echo esc_attr( $aria_author ); ?> role="author">by <?php coauthors_posts_links(); ?></cite>
 			<?php endif; ?>
 
 			<?php if ( is_singular() && spine_has_featured_image() && get_post( get_post_thumbnail_id() )->post_excerpt ) : ?>
